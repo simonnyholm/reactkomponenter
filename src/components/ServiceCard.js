c369342cbc062dc9eedac0992156c619e127553f
Simon Nyholm Sørensen <simonnyholm@htomail>
2022-05-20 15:47:47 +0200

    icons

--- src/components/ServiceCard.js
+++ src/components/ServiceCard.js
@@ -1,8 +1,28 @@
+/** @jsxImportSource @emotion/react */
+import { css } from "@emotion/react";
+
+import { MdOutlineDeliveryDining } from "react-icons/md";
+import { FiArchive } from "react-icons/fi";
+import { BsJournalCheck } from "react-icons/bs";
+import { MdPhone } from "react-icons/md";
+
 const ServiceCard = ({ service, section }) => {
+  const styles = {
+    serviceCard: css``,
+  };
+
   return (
-    <div>
+    <div
+      css={styles.serviceCard}
+      style={{ backgroundColor: service.background }}
+    >
       <div className="imageDiv">
-        <img src="" alt="" className="imageImg" />
+        {service.image === "MdOutlineDeliveryDining" && (
+          <MdOutlineDeliveryDining />
+        )}
+        {service.image === "FiArchive" && <FiArchive />}
+        {service.image === "BsJournalCheck" && <BsJournalCheck />}
+        {service.image === "MdPhone" && <MdPhone />}
       </div>
       <h3>{service.title}</h3>
       <p>{service.body}</p>
